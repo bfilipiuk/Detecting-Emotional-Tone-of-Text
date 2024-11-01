4e2f059d0ab1b22dcad6a0e34b0fbe7be1c5c2e4
bfilipiuk <bartek.filipiuk02@gmail.com>
2024-11-01 20:14:07 +0100

    changed server, now it's getting api url from text file

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 const API_KEY = fs.readFileSync('apikey.txt', 'utf-8').trim();
-const URL = 'https://api.eu-de.natural-language-understanding.watson.cloud.ibm.com/instances/3fa0d7fa-644e-41e5-a71b-68ab6d37c35b';
+const URL = fs.readFileSync('apiurl.txt', 'utf-8').trim();
 
 app.post('/analyze', async (req, res) => {
     try {
